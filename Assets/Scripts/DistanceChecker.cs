f0fc4a5de966023c9b5f94e8456a57633a6c95f7
Astor-S <154661724+Astor-S@users.noreply.github.com>
2025-11-03 21:27:18 +0300

    попробовали задизолвить ешку

--- Assets/Scripts/DistanceChecker.cs
+++ Assets/Scripts/DistanceChecker.cs
@@ -2,11 +2,10 @@ using UnityEngine;
 
 public class DistanceChecker : MonoBehaviour
 {
-    [SerializeField] private Transform _player;
     [SerializeField] private DoorInteractor _doorInteractor; 
     [SerializeField] private Transform _door;
     [SerializeField] private float _distance;
 
     private void Update() =>
-        _doorInteractor.BecomeInteractable(Vector3.Distance(_door.position, _player.transform.position) < _distance);
+        _doorInteractor.BecomeInteractable(Vector3.Distance(_door.position, Player.Position) < _distance);
 }
