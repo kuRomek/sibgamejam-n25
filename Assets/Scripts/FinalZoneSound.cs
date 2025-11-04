03b81497924a41c78581a9e3928b1a0e03f7c2cf
Astor-S <154661724+Astor-S@users.noreply.github.com>
2025-11-04 16:42:02 +0300

    добавлен финальный звук

--- Assets/Scripts/FinalZoneSound.cs
+++ Assets/Scripts/FinalZoneSound.cs
@@ -1,14 +1,23 @@
+using System.Collections;
 using UnityEngine;
 
 public class FinalZoneSound : MonoBehaviour
 {
-    [SerializeField] private AudioClip _audioClip;
-    
-    private AudioSource _audioSource;
+    [SerializeField] private AudioClip _audioClip;  
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private float _delay = 5f;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.TryGetComponent(out Player _))
+            StartCoroutine(PlaySoundAfterDelay(_delay));
+    }
+
+    private IEnumerator PlaySoundAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        if (_audioSource != null && _audioClip != null)
             _audioSource.PlayOneShot(_audioClip);
     }
 }
